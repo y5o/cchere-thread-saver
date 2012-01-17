5d42cf74b74972ff9e38a42578fecd276e8a3b6e
yangg.engr@gmail.com <yangg.engr@gmail.com@e404ef91-bd23-c04e-5c72-8146847461f2>
2012-01-17 04:53:54 +0000

    first upload

---
+++ trunk/cchstrip.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+import codecs
+import datetime
+from lxml import html
+import re
+
+def getAuthor(ele):
+  """ get author from element
+  """
+  r = ele.xpath('div/div/a')
+  return r[0].text
+  
+
+def onefile(i, baseurl, enc = "utf-8"):
+  """ get element list from file
+  """
+  f = codecs.open(str(i)+".html", 'r', enc)
+  fulltext = f.read()
+  doc = html.fromstring(fulltext) 
+  doc.make_links_absolute(baseurl)
+  alllist = doc.find_class("pContent")  # list of posts
+  f.close()
+  return alllist
+
+def getDateTime(ele):
+  """ get a datetime obj from element
+  """
+  r = ele.xpath('div/div')
+  timestr = r[0].text_content()
+  patn = "\d\d\d\d-\d\d-\d\d\s\d\d:\d\d:\d\d"
+  match = re.search(patn, timestr)
+  if match:
+    timestr = match.group()
+    ret = datetime.datetime.strptime(timestr, "%Y-%m-%d %H:%M:%S")
+    return ret
+  else:
+    print "datetime extract error!"
+
+def generateHTML(allposts, outfilename = "all.html", enc = "utf-8"):
+  """ put all posts in the given list in to a simple 
+      html file.
+  """
+  begstr = "<html><body>"
+  allstr = ""
+  for p in allposts:
+    allstr = allstr + html.tostring(p)
+  endstr = "</body></html>"
+  f = codecs.open(outfilename, 'w', enc)
+  f.write(begstr + allstr + endstr)
+  f.close()
+
+def sortByDate(allposts):
+  allposts.sort(key = getDateTime)
+
+if __name__ == '__main__':
+  
+  maxpage = 243
+  authorToPick = "Emyn"
+  
+  baseurl = "http://www.ccthere.com"
+  postlist = []
+  # read in all posts to one list
+  for i in range(1, maxpage+1):
+    postlist = postlist + onefile(i, baseurl)
+  
+  sameauthorlist = [ele for ele in postlist if authorToPick == getAuthor(ele)]
+#  print len(sameauthorlist)
+#  for ele in postlist:
+#    print getDateTime(ele)
+#  for ele in sameauthorlist:
+#    print getDateTime(ele)
+  
+  sortByDate(sameauthorlist)
+  generateHTML(sameauthorlist) 
+#  for ele in sameauthorlist:
+#    print getDateTime(ele)
+
